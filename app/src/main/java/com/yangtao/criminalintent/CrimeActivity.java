4e63a957a6b4055c0fb95482cd959cdffed567bc
Yangtao <yangtao17@foxmail.com>
2016-05-10 10:14:29 +0800

    完成第8章内容

---
+++ app/src/main/java/com/yangtao/criminalintent/CrimeActivity.java
@@ -0,0 +1,23 @@
+package com.yangtao.criminalintent;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment==null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragmentContainer,fragment).commit();
+        }
+    }
+}
