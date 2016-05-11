9e1864b77966855327393d1931800ece7c22e2d6
Yangtao <yangtao17@foxmail.com>
2016-05-11 09:39:43 +0800

    完成第11章内容-ViewPager加入

--- app/src/main/java/com/yangtao/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/yangtao/criminalintent/CrimeListFragment.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -18,7 +17,7 @@ import java.util.ArrayList;
  * Created by YANGTAO on 2016/5/10.
  */
 public class CrimeListFragment extends ListFragment {
-   private static String TAG = "CrimeListFragment";
+//   private static String TAG = "CrimeListFragment";
     private ArrayList<Crime> mCrimes;
 
     @Override
@@ -36,7 +35,8 @@ public class CrimeListFragment extends ListFragment {
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         Crime c =( (CrimeAdapter)getListAdapter()).getItem(position);
-        Intent i = new Intent(getActivity(),CrimeActivity.class);
+//        Intent i = new Intent(getActivity(),CrimeActivity.class);
+        Intent i = new Intent(getActivity(),CrimePagerActivity.class);
         i.putExtra(CrimeFragment.EXTRA_CRIME_ID,c.getId());
         startActivity(i);
     }
---
+++ app/src/main/java/com/yangtao/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,70 @@
+package com.yangtao.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+/**
+ * Created by YANGTAO on 2016/5/11.
+ */
+public class CrimePagerActivity extends AppCompatActivity {
+    private ViewPager mViewPager;
+    private ArrayList<Crime> mCrimes;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mViewPager = new ViewPager(this);
+        mViewPager.setId(R.id.viewPager);
+        setContentView(mViewPager);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+
+        FragmentManager fm =getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentPagerAdapter(fm) {
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+        final UUID crimeId =  (UUID)getIntent().getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+        for (int i = 0; i<mCrimes.size();i++){
+            if (mCrimes.get(i).getId().equals(crimeId)){
+                mViewPager.setCurrentItem(i);
+                break;
+            }
+        }
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                Crime crime = mCrimes.get(position);
+                if (crime.getTitle() != null){
+                    setTitle(crime.getTitle());
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+}
